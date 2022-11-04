c9b95f1253a6cbb28fbc28a45b6d911501596218
Thomas Guillemont <thomas.guillemont@hotmail.fr>
2022-11-04 14:45:44 +0100

    INIT

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="./assets/img/pnj/cv.jpg">
+    <meta name="description" content="ENTREPRISE - Votre site internet simplement">
+    <title>ENTREPRISE - Votre site internet simplement</title>
+    <link rel="icon" type="image/x-icon" href="./assets/img/thomasguiillemont.jpg" />
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- CSS -->
+    <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-xl text-center fixed-top" aria-label="Barre de navigation" id="header">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <h1 class="fs-4 fw-bold">ENTREPRISE</h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Basculer la navigation">
+                <span class="fw-bold">Menu</span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Element
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Element
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- main -->
+    <main>
+        <div class="container-fluid mt-5">
+            <div class="row">
+                <div class="col">
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                    <p>Blablabla</p>
+                </div>
+            </div>
+    </main>
+    <!-- footer -->
+    <footer class="container-fluid">
+        <div class="row">
+            <div class="col text-center" id="footer">
+                <b>ENTREPRISE</b> - Tous droits réservés &copy
+                <?= date("Y") ?>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
